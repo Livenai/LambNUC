5f745c10e6a35baeb68e9cf95c89031a47a09fd5
Alberto <albertoserranoleon@gmail.com>
2019-12-12 13:45:04 +0100

    Added a mock server to test without the actual weighing machine

---
+++ src/mock_server.py
@@ -0,0 +1,37 @@
+import http.server
+import socketserver
+import time
+import os
+import numpy as np
+from json import dumps
+from threading import Thread
+
+def server():
+    PORT = 8000
+
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
+def updating():
+    print("updating html")
+    path = os.path.join(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))), "index.html")
+    print(path)
+    while True:
+        ts = float(time.time())
+        file = {"time": ts, "current_weight": {"time": ts, "value": float(4.1*np.random.random_sample())}, "valid_weight": {"time": ts, "value": float(6.7*np.random.random_sample())}}
+        with open(path, "w") as f:
+            f.write(dumps(file, indent=4))
+        print("updated")
+        time.sleep(6)
+        
+t_server = Thread(target=server)
+t_updating = Thread(target=updating)
+
+t_server.start()
+t_updating.start()
+    
+while True:
+    pass
